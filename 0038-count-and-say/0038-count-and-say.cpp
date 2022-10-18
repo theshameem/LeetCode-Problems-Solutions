6100d08b4b510dcabafa2466ba0ddb13faf441fa
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-18 17:10:13 +0600

    Time: 18 ms (47.09%), Space: 6.7 MB (42.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    string findNTHSay(string s){
-        string ans = "";
-        for(int i = 0; i < s.size(); ){
-            int j = i, cnt = 0;
-            while(j < s.size() && s[i] == s[j]){
-                ++cnt, ++j;
-            }
-            ans += to_string(cnt) + s[i];
-            i = j;
-        }
-        return ans;
-    }
-    
     void findNthSayByRecursion(int n, string &ans){
         if(n == 1){
             ans = "1";
             return;
         }
+        
         findNthSayByRecursion(n - 1, ans);
+        
         string s = ans;
         ans = "";
         for(int i = 0; i < s.size(); ){
@@ -32,12 +21,6 @@ public:
     }
     
     string countAndSay(int n) {
-        string ans = "1";
-        
-        for(int i = 1; i < n; i++){
-            ans = findNTHSay(ans);
-        }
-        
         string tmp = "";
         
         findNthSayByRecursion(n, tmp);
