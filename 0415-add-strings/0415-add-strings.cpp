a4356f7c09f75f7d7c68ad1d6e6c68fcf9b49922
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-18 23:43:45 +0600

    Time: 4 ms (81.33%), Space: 6.8 MB (67.72%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int carry = 0;
-        vector<int> v;
-        while(num1.size() && num2.size()){
-            int x = num1.back() - '0';
-            int y = num2.back() - '0';
-            v.push_back((x + y + carry) % 10);
-            carry = (x + y + carry) / 10;
-            
-            num1.pop_back();
-            num2.pop_back();
-        }
-        
-        while(num1.size()){
-            int x = num1.back() - '0';
-            v.push_back((x + carry) % 10);
-            carry = (x + carry) / 10;
-            
-            num1.pop_back();
-        }
-        
-        while(num2.size()){
-            int x = num2.back() - '0';
-            v.push_back((x + carry) % 10);
-            carry = (x + carry) / 10;
-            
-            num2.pop_back();
-        }
-        if(carry) v.push_back(carry);
-        
-        reverse(v.begin(), v.end());
-        
+        int sum = 0, i = num1.size() - 1, j = num2.size() - 1;
         string ans = "";
         
-        for(auto i: v) ans += to_string(i);
+        while(i >= 0 && j >= 0){
+            sum += ((num1[i--] - '0') + (num2[j--] - '0'));
+            ans += (char(sum % 10 + '0'));
+            sum /= 10;
+        }
+        
+        while(i >= 0){
+            sum += (num1[i--] - '0');
+            ans += char(char(sum % 10 + '0'));
+            sum /= 10;
+        }
+        
+        while(j >= 0){
+            sum += (num2[j--] - '0');
+            ans += char(char(sum % 10 + '0'));
+            sum /= 10;
+        }
+        if(sum > 0) ans += char(sum % 10 + '0');
+        
+        reverse(ans.begin(), ans.end());
         
         return ans;
     }
