049bbee423a98cc167ca1ffe4d471d815fc2d1a7
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-19 21:11:06 +0600

    Time: 792 ms (5.54%), Space: 71.5 MB (99.10%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int countWords(string s){
-        int space = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == ' ') ++space;
-        }
-        return ++space;
-    }
-    
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
         map<string, int> mp;
         int mx = 0;
         string ans = "";
         for(int i = 0; i < messages.size(); i++){
-            int wordsCount = countWords(messages[i]);
-            mp[senders[i]] += wordsCount;            
-        }
-        
-        for(auto i: mp){
-            if(i.second == mx){
-                if(i.first > ans){
-                    ans = i.first;
+            int wordsCount = count(messages[i].begin(), messages[i].end(), ' ') + 1;
+            mp[senders[i]] += wordsCount;
+            
+            if(mp[senders[i]] > mx){
+                mx = mp[senders[i]];
+                ans = senders[i];
+            } else if(mp[senders[i]] == mx){
+                if(senders[i] > ans){
+                    ans = senders[i];
                 }
-            } else if(i.second > mx){
-                mx = i.second;
-                ans = i.first;
             }
         }
+        
+        // for(auto i: mp){
+        //     if(i.second == mx){
+        //         if(i.first > ans){
+        //             ans = i.first;
+        //         }
+        //     } else if(i.second > mx){
+        //         mx = i.second;
+        //         ans = i.first;
+        //     }
+        // }
         return ans;
     }
 };
