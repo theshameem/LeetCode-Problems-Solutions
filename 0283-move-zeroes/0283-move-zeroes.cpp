cb2e55088b790bbd04957ed4aa89635b1cb5485d
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-21 18:13:07 +0600

    Time: 14 ms (99.74%), Space: 19.3 MB (14.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroPointer = -1, otherPointer = 0;;
+        int zeroPointer = 0, otherPointer = 0;;
         while(otherPointer < nums.size()){
-            if(nums[otherPointer] == 0 && zeroPointer == -1){
-                zeroPointer = otherPointer;
-                ++otherPointer;
-                continue;
-            }
-            
-            if(nums[otherPointer] != 0 && zeroPointer != -1){
+            if(nums[otherPointer] != 0){
                 swap(nums[otherPointer], nums[zeroPointer]);
                 ++otherPointer, ++zeroPointer;
             } else {
