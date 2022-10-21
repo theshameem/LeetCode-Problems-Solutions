37c93ea9a439d5b866eb2124e688cd330ccf7f80
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-21 15:04:59 +0600

    Time: 511 ms (13.07%), Space: 80.5 MB (15.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int len = nums.size();
-        if(len == 1) return false;
-        
-        int i = 0, j = 1;
-        map<int, int> mp;
-        mp[nums[0]]++;
-        
-        while(i < j && j < len){   
+        set<int> st;
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++){
             if(abs(i - j) > k){
-                mp[nums[i]]--;
-                if(mp[nums[i]] < 1) mp.erase(nums[i]);
-                i++;
-                continue;
+                st.erase(nums[j]);
+                j++;
             }
-            
-            if(mp[nums[j]] > 0) return true;
-            
-            if(j + 1 == len){
-                mp[nums[i]]--;
-                if(mp[nums[i]] < 1) mp.erase(nums[i]);
-                ++i;
-            } else {
-                mp[nums[j]]++;
-                ++j;
-            }
+            if(st.find(nums[i]) != st.end()) return true;
+            st.insert(nums[i]);
         }
         return false;
     }
