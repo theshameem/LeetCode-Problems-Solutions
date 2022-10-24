f97a489c23d763b44a3eb655a06d6ead414901e7
theshameem <37625460+theshameem@users.noreply.github.com>
2022-10-24 20:22:47 +0600

    Time: 742 ms (13.36%), Space: 190.9 MB (13.18%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int n;
+    vector<string> ar;
+    
+    bool findNoDuplicate(string s, string t){
+        map<char, int> mp;
+        for(char i: s) mp[i]++;
+        for(char i: t) mp[i]++;
+        
+        for(auto i: mp){
+            if(i.second > 1) return false;
+        }
+        
+        return true;
+    }
+    
+    int solve(int pos, string s){
+        if(pos == n){
+            // cout << s << endl;
+            // if(!findNoDuplicate(s, "")) return 0;
+            return s.size();
+        }
+        int res = 0;
+        
+        if(findNoDuplicate(s, ar[pos])){
+            res += solve(pos + 1, s + ar[pos]);
+        }
+        int res2 = solve(pos + 1, s + "");
+        
+        return max(res, res2);
+    }
+    
+    int maxLength(vector<string>& arr) {
+        n = arr.size();
+        ar = arr;
+        return solve(0, "");
+    }
+};
