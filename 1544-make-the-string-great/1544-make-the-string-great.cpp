2626ad4c0cb75761920e943986327f6eae38fcfe
theshameem <37625460+theshameem@users.noreply.github.com>
2022-11-08 19:15:56 +0600

    Time: 13 ms (5.98%), Space: 6.5 MB (54.48%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        vector<char> st;
+        for(int i = 0; i < s.size(); i++){
+           if(st.size() && abs(st.back() - s[i]) == 32) st.pop_back();
+           else st.push_back(s[i]);
+        }
+        string ans(st.begin(), st.end());
+        
+        return ans;
+    }
+};
