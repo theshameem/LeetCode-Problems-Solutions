15f9d7ce3cb23ca2d0be4788d60343973813e63c
theshameem <37625460+theshameem@users.noreply.github.com>
2022-11-14 22:55:24 +0600

    Time: 102 ms (77.38%), Space: 19.1 MB (40.22%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int parent[100000];
+    void makeUnion(int u, int v){
+        int uParent = findParent(u);
+        int vParent = findParent(v);
+        parent[uParent] = vParent;
+    }
+    
+    int findParent(int n){
+        if(parent[n] == n) return n;
+        return parent[n] = findParent(parent[n]);
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int mxRow = 0, mxCol = 0;
+        for(auto i: stones){
+            mxRow = max(mxRow, i[0]);
+            mxCol = max(mxCol, i[1]);
+        }
+        for(int i = 0; i < mxRow + mxCol + 10; i++) parent[i] = i;
+        
+        unordered_map<int, int> stoneNodes;
+        for(auto it: stones){
+            int nodeRow = it[0];
+            int nodeCol = it[1] + mxRow + 1;
+            makeUnion(nodeRow, nodeCol);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+        int cnt = 0;
+        for(auto it: stoneNodes){
+            if(findParent(it.first) == it.first) ++cnt;
+        }
+        // for(int i = 0; i <= mxRow + mxCol; i++){
+        //     cout << parent[i] << " ";
+        // }
+        // cout << endl;
+        // unordered_set<int> stoneNode;
+        // for(int i = 0; i <= mxRow + mxCol; i++){
+        //     stoneNode.insert(findParent(i));
+        // }
+        return n - cnt;
+    }
+};
