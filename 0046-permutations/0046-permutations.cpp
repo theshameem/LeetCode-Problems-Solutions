d9c6c016ab3e76506b4f067a215a6cc4579874ef
theshameem <37625460+theshameem@users.noreply.github.com>
2022-11-15 22:29:08 +0600

    Time: 7 ms (47.45%), Space: 8.9 MB (6.95%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,9 +1,8 @@
 class Solution {
+    private:
+        vector<vector<int>> allPermutations;
+        vector<int> takenValue;
 public:
-    
-    vector<vector<int>> allPermutations;
-    vector<int> flag;
-    
     void generatePermutations(vector<int> nums, vector<int> currPermutation){
         if(currPermutation.size() == nums.size()){
             allPermutations.push_back(currPermutation);
@@ -11,17 +10,17 @@ public:
         }
         
         for(int i = 0; i < nums.size(); i++){
-            if(flag[i] == 1) continue;
-            flag[i] = 1;
+            if(takenValue[i] == 1) continue;
+            takenValue[i] = 1;
             currPermutation.push_back(nums[i]);
             generatePermutations(nums, currPermutation);
-            flag[i] = 0;
+            takenValue[i] = 0;
             currPermutation.pop_back();
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        flag.resize(10, 0);
+        takenValue.resize(10, 0);
         generatePermutations(nums, {});
         
         return allPermutations;
